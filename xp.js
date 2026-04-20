e3b65a6c7d8646f97e5eebb558370e23cdd213b1
Sidharth Vijayan <sidharth.vijayan2111@gmail.com>
2026-04-20 21:19:50 +0530

    Update xp.js

--- xp.js
+++ xp.js
@@ -1,12 +1,9 @@
-function updateXP(consumed, target) {
-  chrome.storage.local.get(["xp"], (data) => {
-    let xp = data.xp || 0;
-
-    if (consumed <= target) xp += 20;
-    else xp -= 10;
-
-    if (xp < 0) xp = 0;
-
-    chrome.storage.local.set({ xp });
+function updateXP(consumed,target){
+  chrome.storage.local.get(["xp"],(data)=>{
+    let xp = data.xp||0;
+    if(consumed<=target) xp+=20;
+    else xp-=10;
+    if(xp<0) xp=0;
+    chrome.storage.local.set({xp});
   });
 }
