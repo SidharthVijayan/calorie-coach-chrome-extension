3247a62ea72d63ae91e8e8c88b0fe874a39ed229
Sidharth Vijayan <sidharth.vijayan2111@gmail.com>
2026-04-20 21:21:48 +0530

    Update foodDatabase.js

--- foodDatabase.js
+++ foodDatabase.js
@@ -1,9 +1,8 @@
 const FOOD_DB = {
-  "biryani": 400,
-  "egg": 78,
-  "pizza": 280,
-  "burger": 300,
-  "fries": 365,
-  "rice": 200,
-  "dal": 180
+  "egg":78,
+  "biryani":400,
+  "pizza":280,
+  "burger":300,
+  "fries":365,
+  "rice":200
 };
